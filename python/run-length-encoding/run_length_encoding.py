70ae0f0d67e4e17971396fd4244c4d76bb5bee6d
Rosyid Haryadi <rosyid_haryadi@yahoo.com>
2017-05-12 08:12:30 +0700

    rle encode

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -1,6 +1,15 @@
+from itertools import groupby
+
 def decode():
     pass
 
 
 def encode(msg):
-    pass
+    encoded = []
+    for k,g in groupby(msg):
+        length = len(list(g))
+        if length > 1:
+            encoded.append(str(length))
+        encoded.append(k)
+    return ''.join(encoded)
+
