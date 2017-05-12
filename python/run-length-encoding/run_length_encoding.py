64609f3dad6ba2c80675079ca37b125165eb217d
Rosyid Haryadi <rosyid_haryadi@yahoo.com>
2017-05-12 12:51:24 +0700

    rle decode

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -1,8 +1,18 @@
 from itertools import groupby
 
-def decode():
-    pass
-
+def decode(msg):
+    # 'X10YZ3X' will be grouped as ['X','10','YZ','3','X']
+    msg = [''.join(c) for _,c in groupby(msg, key=str.isdigit)]
+    decoded = []
+    for i,item in enumerate(msg):
+        if item.isdigit():
+            # [...,'3',BN',..] will be
+            # [...,'BB','BN',...]
+            decoded.append((int(item)-1)*msg[i+1][0])
+        else:
+            decoded.append(item)
+    return ''.join(decoded)
+        
 
 def encode(msg):
     encoded = []
