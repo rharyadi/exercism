7ef7abdff6a55c6d9b9a812addfd09c5c2630575
Rosyid Haryadi <rosyid_haryadi@yahoo.com>
2017-05-21 19:42:35 +0700

    rotational cipher solved

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,21 @@
-def rotate():
-    pass
+import string
+
+def rotate(text,rot):
+    lower_map = dict(zip(
+                string.ascii_lowercase,
+                string.ascii_lowercase[rot:26] + string.ascii_lowercase[0:rot]
+                ))
+    upper_map = dict(zip(
+                string.ascii_uppercase,
+                string.ascii_uppercase[rot:26] + string.ascii_uppercase[0:rot]
+                ))
+
+    rotated_text = []
+    for c in text:
+        if c.islower():
+            c = lower_map[c]
+        if c.isupper():
+            c = upper_map[c]
+        rotated_text.append(c)
+
+    return ''.join(rotated_text)
