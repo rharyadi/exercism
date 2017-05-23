b320b099008ac1d20904fe17ff2bad55891cffef
Rosyid Haryadi <rosyid_haryadi@yahoo.com>
2017-05-23 21:40:14 +0700

    fix potential rounding error

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -3,13 +3,13 @@ reference: https://trans4mind.com/personal_development/mathematics/series/sumNat
 """
 
 def square_of_sum(n):
-    s = (n/2) * (n+1)
-    return int(s**2)
+    s = (n * (n+1))/2
+    return s**2
 
 def sum_of_squares(n):
     n2 = n**2
     n3 = n**3
-    return round(n3/3 + n2/2 + n/6)
+    return (2*n3 + 3*n2 + n)/6
 
 def difference(n):
     return abs(square_of_sum(n) - sum_of_squares(n))
