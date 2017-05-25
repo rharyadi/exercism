62e42caa59bb9dd59802299023f7cd18f31080d1
Rosyid Haryadi <rosyid_haryadi@yahoo.com>
2017-05-25 11:08:54 +0700

    fix rotational cipher

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -3,19 +3,18 @@ import string
 def rotate(text,rot):
     lower_map = dict(zip(
                 string.ascii_lowercase,
-                string.ascii_lowercase[rot:26] + string.ascii_lowercase[0:rot]
+                string.ascii_lowercase[rot:] + string.ascii_lowercase[:rot]
                 ))
     upper_map = dict(zip(
                 string.ascii_uppercase,
-                string.ascii_uppercase[rot:26] + string.ascii_uppercase[0:rot]
+                string.ascii_uppercase[rot:] + string.ascii_uppercase[:rot]
                 ))
 
+    charmap = {**lower_map, **upper_map}
+
     rotated_text = []
+
     for c in text:
-        if c.islower():
-            c = lower_map[c]
-        if c.isupper():
-            c = upper_map[c]
-        rotated_text.append(c)
+        rotated_text.append(charmap.get(c,c))
 
     return ''.join(rotated_text)
